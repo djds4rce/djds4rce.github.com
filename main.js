d2ba24626966b4dba7c3958fdddc0e65ba58c90b
computer <djd4rce@gmail.com>
2012-07-20 23:27:34 +0530

    changed color combination| made inputing text work| added font and styles

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
   $('#commandin').submit(function(event){
     event.preventDefault();
-    $('#stdout').append("\nhello");
-    $('#stdout').html(txt)
+    $('#stdout-executed').append("<p>#"+$('#in').val());
+    $('#stdout-executed').append("<p>hello</p>");
     window.scrollBy(0, 122500);                                              
     $('#in').val('');
     $('#in').focus();
