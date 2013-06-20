6aa1cfa710400fb67f3c4d3f03e8572a03996b26
djds4rce <djd4rce@gmail.com>
2013-06-20 20:37:18 +0530

    settimeout works properly

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-function TerminalCtrl($scope,$http){
+function TerminalCtrl($scope,$http,$timeout){
   $scope.commands = [];
   var urls = {
     blog: 'http://djds4rce.wordpress.com',
@@ -9,7 +9,8 @@ function TerminalCtrl($scope,$http){
 
   $http.get('text.json').success(function(data) {
     $scope.results = data;
-    $scope.enter('whoami');
+    $timeout(function(){$scope.comm = 'whoami';},1000);
+    $timeout(function(){$scope.enter('whoami');},2000);
   });
 
   $scope.enter = function(comm){
@@ -26,12 +27,13 @@ function TerminalCtrl($scope,$http){
           $scope.commands = $scope.commands.concat({'key':comm,'output':output});
       }
 
-        url = urls[comm];
-        if (!(url==undefined)){
-          window.open(url);
-          return false;
-        }
+      url = urls[comm];
+      if (!(url==undefined)){
+        window.open(url);
+        return false;
+      }
   }
+
 
   $scope.focus = function(){
     $('#in').focus();
